d989f2b23a80b7deb83b28527171d9bb97645dc5
zubair <zubairabuhasan@gmail.com>
2018-10-07 09:58:52 +0800

    Upload and download files

---
+++ AspDotNetTraining/Controllers/UploadsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspDotNetTraining.Controllers
+{
+    public class UploadsController : Controller
+    {
+        // GET: Uploads
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Store(UploadsCustomViewModel model, HttpPostedFileBase file)
+        {
+            const string basePath = "~/Uploads";
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+
+                if (fileName != null)
+                {
+                    var relativePath = Server.MapPath(basePath);
+
+                    // Create directory if does not already exist
+                    Directory.CreateDirectory(relativePath);
+
+                    // Save file
+                    var filePath = Path.Combine(relativePath, fileName);
+                    file.SaveAs(filePath);
+
+                    // Map to model - and maybe store in database
+                    model.Path = Url.Content(Path.Combine(basePath, fileName));
+
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Download(string filePath)
+        {
+            const string basePath = "~/Uploads";
+            
+            var fileName = Path.GetFileName(filePath);
+
+            if (fileName == null) return HttpNotFound("File Not Found");
+
+            // Get MIME type
+            var mimeType = MimeMapping.GetMimeMapping(fileName);
+
+            var destinationPath = Path.Combine(Server.MapPath(basePath), fileName);
+
+            return File(destinationPath, mimeType, fileName);
+        }
+    }
+
+    public class UploadsCustomViewModel
+    {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+    }
+}
